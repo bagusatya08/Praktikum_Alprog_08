691f9af3dd72c74592a35e739f9894ebf9c416ae
bagusatya <bagusatya08@gmail.com>
2022-03-14 03:47:39 +0800

    git ignore, update on modul3 nomor 1, and upload new modul 3 nomor 4

--- modul3/modul3_nomor1_kelompok8.c
+++ modul3/modul3_nomor1_kelompok8.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<time.h>
 #include<stdlib.h>
+#include<string.h>
 
 void tukar(int *a, int *b){
     int t = *a;
@@ -25,13 +26,13 @@ void linear_search(int kumpulan[], int dicari){
     while (dicari!=kumpulan[n]){
         n++;
     }
-    printf("ANGKA DICARI ADA PADA INDEKS KE-%d\n", n);
+    printf("TERLETAK PADA INDEKS KE-%d\n", n);
 }
 
 void random_number(int kumpulan[],int banyak_data){
 
     int angka;
-    int low=0, high=20;
+    int low=0, high=180000;
     srand(time(NULL));
 
 //memasukan nilai random ke dalam array
@@ -61,47 +62,116 @@ void insertion_sort(int kumpulan[], int banyak_data){
 
 int main(){
 
-    int kumpulan[]={};
-    int banyak_data;
-    int dicari;
-    int pilihan_awal;
+    int kumpulan[70000]={};
+    int banyak_data, dicari, pilihan_program=0, pilihan_searching=0, pilihan_sorting=0;
+    char mengulang[10];
 
-    printf("MASUKAN BANYAK DATA : ");
-    scanf("%d", &banyak_data);
-    system("cls");
+    do{
+        system("clear");
+        printf("\n+------------------------------+\n");
+        printf("| MASUKAN BANYAK DATA   : ");
+        scanf("%d", &banyak_data);
+        printf("+------------------------------+\n");
+        system("clear");
 
-    printf("DATA AWAL : ");
-    random_number(kumpulan, banyak_data);
-    printf("\n");
+        printf("+------------------------------+\n");
+        printf("| LIST PILIHAN PROGRAM         |\n");
+        printf("+------------------------------+\n");
+        printf("| 1. SEARCHING                 |\n");
+        printf("| 2. SORTING                   |\n");
+        printf("+------------------------------+\n");
+        printf("| MASUKAN PILIHAN PROGRAM   : ");
+        scanf("%d", &pilihan_program);
 
-    scanf("%d", &pilihan_awal);
+        system("clear");
+        if (pilihan_program==1 || pilihan_program==2){
+            if (pilihan_program==1){
+                printf("+------------------------------+\n");
+                printf("| LIST PILIHAN SEARCHING       |\n");
+                printf("+------------------------------+\n");
+                printf("| 1. SEQUENTIAL SEARCH         |\n");
+                printf("| 2. BINARY SEARCH             |\n");
+                printf("+------------------------------+\n");
+                printf("| MASUKAN PILIHAN SEARCHING : ");
+                scanf("%d", &pilihan_searching);
+                system("clear");
 
-    if (pilihan_awal==1){
-        printf("DATA YANG TELAH DIURUTKAN : ");
-        bubble_sort(kumpulan, banyak_data);
+                if (pilihan_searching==1||pilihan_searching==2){
+                    
+                    //random number generator
+                    printf("\n+------------------------------+\n");
+                    printf("| DATA AWAL                    |");
+                    printf("\n+------------------------------+\n");
+                    random_number(kumpulan, banyak_data);
+                    printf("\n");
 
-        for (int z = 0; z < banyak_data; z++){
-            printf("%d ", kumpulan[z]);
+                    printf("\n+------------------------------+\n");
+                    printf("| MASUKAN ANGKA SEARCHING      |\n");
+                    printf("+------------------------------+\n");
+                    scanf("%d", &dicari);
+                    printf("\n+------------------------------+\n");
+                    printf("| HASIL SEARCHING              |");
+                    printf("\n+------------------------------+\n");
+
+                    if (pilihan_searching==1){
+                        linear_search(kumpulan, dicari);
+                    } else {
+                        printf("on progress\n");
+                    }
+                } else{
+                    printf("MASUKAN PILIHAN YANG SESUAI\n");
+
+                }
+                
+            } else if (pilihan_program==2){
+                printf("+------------------------------+\n");
+                printf("| LIST PILIHAN SORTING         |\n");
+                printf("+------------------------------+\n");
+                printf("| 1. BUBBLE SORT               |\n");
+                printf("| 2. INSERTION SORT            |\n");
+                printf("| 3. QUICK SORT                |\n");
+                printf("+------------------------------+\n");    
+                printf("| MASUKAN PILIHAN SORTING   : ");
+                scanf("%d", &pilihan_sorting);
+                system("clear");
+
+                if (pilihan_sorting>0&&pilihan_sorting<4){
+                    //random number generator
+                    printf("\n+------------------------------+\n");
+                    printf("| DATA AWAL                    |");
+                    printf("\n+------------------------------+\n");
+                    random_number(kumpulan, banyak_data);
+                    printf("\n");
+
+                    printf("\n+------------------------------+\n");
+                    printf("| HASIL SORTING                |");
+                    printf("\n+------------------------------+\n");
+
+                    if (pilihan_sorting==1){
+                        bubble_sort(kumpulan, banyak_data);
+                    } else if (pilihan_sorting==2){
+                        insertion_sort(kumpulan, banyak_data);
+                    } else {
+                    /* code */
+                    }
+
+                    for (int z = 0; z < banyak_data; z++){
+                        printf("%d ", kumpulan[z]);
+                    }
+                } else{
+                    printf("MASUKAN PILIHAN YANG SESUAI\n");
+                }
+            }
+        //menghitung waktu sorting
+        int waktu_ticks= clock();
+        printf("\n\nWAKTU PROSES = %f DETIK\n", (float)waktu_ticks/ CLOCKS_PER_SEC);
+
+        } else{
+            printf("MASUKAN PILIHAN YANG SESUAI\n");
         }
-    } else if(pilihan_awal==2){
-        printf("MASUKAN ANGKA YANG DICARI :");
-        scanf("%d", &dicari);
-        linear_search(kumpulan, dicari);
-    } else{
-        printf("DATA YANG TELAH DIURUTKAN : ");
-        insertion_sort(kumpulan, banyak_data);
-
-        for (int z = 0; z < banyak_data; z++){
-            printf("%d ", kumpulan[z]);
-        }
-    }
-
-
-    printf("\n");
-
-//menghitung waktu sorting
-    int waktu_ticks= clock();
-    printf("\n\nWAKTU PROSES = %f DETIK\n", (float)waktu_ticks/ CLOCKS_PER_SEC);
+    printf("MENGULANG PROGRAM (Y/y) : ");
+    scanf(" %c", mengulang);
+    }while(strcmp(mengulang, "y")==0 || strcmp(mengulang,"Y")==0);
 
     return 0;
 }
---
+++ modul3/modul3_nomor4_kelompok8.c
@@ -0,0 +1,127 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+#include<string.h>
+
+void tukar(int *a, int *b){
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int bubble_sort_pointer(int kumpulan[], int banyak_data){
+    int j;
+    for ( int i = 0; i < banyak_data; i++){
+        for (j = 0; j < banyak_data-i-1; j++){
+            if (kumpulan[j]>kumpulan[j+1]){
+                tukar(&kumpulan[j], &kumpulan[j+1]);
+            }  
+        } 
+    } 
+    return kumpulan[j];
+}
+
+int bubble_sort_biasa(int kumpulan[], int banyak_data){
+
+    int tukar,j;
+
+    for ( int i = 0; i < banyak_data; i++){
+        for ( j = 0; j < banyak_data-i-1; j++){
+            if (kumpulan[j]>kumpulan[j+1]){
+                tukar=kumpulan[j];
+                kumpulan[j]=kumpulan[j+1];
+                kumpulan[j+1]=tukar;
+            }  
+        } 
+    }
+    return kumpulan[j];
+}
+
+void random_number(int kumpulan[],int banyak_data){
+
+    int angka;
+    int low=0, high=180000;
+    srand(time(NULL));
+
+//memasukan nilai random ke dalam array
+    for (int i = 0; i <banyak_data; i++){
+        angka=(rand() % (high-low+1)) + low;
+        kumpulan[i]=angka;
+        printf("%d ", kumpulan[i]);
+    }
+}
+
+int main(){
+
+    int kumpulan[70000];
+    int banyak_data, pilih_jenis;
+    char mengulang[10];
+
+    do{
+        system("clear");
+        printf("\n+------------------------------+\n");
+        printf("| PILIHAN BANYAK DATA          |\n");
+        printf("+------------------------------+\n");
+        printf("| 1. 1000 DATA                 |\n");
+        printf("| 2. 16000 DATA                |\n");
+        printf("| 3. 64000 DATA                |\n");
+        printf("+------------------------------+\n");    
+        printf("| MASUKAN PILIHAN SORTING   : ");
+        scanf("%d", &banyak_data);
+        printf("+------------------------------+\n");
+
+        if (banyak_data>0 && banyak_data<4){
+            if (banyak_data==1){
+                banyak_data=1000;
+            } else if (banyak_data==2){
+                banyak_data=16000;
+            } else{
+                banyak_data=64000;
+            }
+
+            printf("\n+------------------------------+\n");
+            printf("| LIST PILIHAN SORTING         |\n");
+            printf("+------------------------------+\n");
+            printf("| 1. BUBBLE SORT NORMAL        |\n");
+            printf("| 2. BUBBLE SORT POINTER       |\n");
+            printf("+------------------------------+\n");    
+            printf("| MASUKAN PILIHAN SORTING   : ");
+            scanf("%d", &pilih_jenis);
+            printf("+------------------------------+\n");
+
+            printf("\n+------------------------------+\n");
+            printf("| DATA AWAL                    |");
+            printf("\n+------------------------------+\n");
+            random_number(kumpulan, banyak_data);
+
+            printf("\n+------------------------------+\n");
+            printf("| HASIL SORTING                |");
+            printf("\n+------------------------------+\n");
+
+            if (pilih_jenis==1||pilih_jenis==2){
+                if (pilih_jenis==1){
+                    bubble_sort_biasa(kumpulan, banyak_data);
+                } else {
+                    bubble_sort_pointer(kumpulan, banyak_data);
+                }
+                
+                for (int z = 0; z < banyak_data; z++){
+                    printf("%d ", kumpulan[z]);
+                }
+
+                //menghitung waktu sorting
+                int waktu_ticks= clock();
+                printf("\n\nWAKTU PROSES = %f DETIK\n", (float)waktu_ticks/ CLOCKS_PER_SEC);
+            } else{
+                printf("MASUKAN SESUAI YANG TERTERA\n");
+            }
+        } else{
+            printf("MASUKAN SESUAI YANG TERTERA\n");
+        }
+        printf("MENGULANG PROGRAM? (y/Y) : ");
+        scanf(" %c", mengulang);
+    }while(strcmp(mengulang,"y")==0||strcmp(mengulang,"Y")==0);
+
+
+    return 0;
+}
