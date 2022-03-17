1cc5953da81a7581493b4264e483900fe00e3790
Jesica Syalomitha <95263114+Integer17@users.noreply.github.com>
2022-03-18 03:00:51 +0800

    Add files via upload

--- modul1/modul1_nomor1_kelompok8.c
+++ modul1/modul1_nomor1_kelompok8.c
@@ -1,88 +1,92 @@
+// Nama 	: Jesica Syalomitha Sule Ulang
+// NIM 		: 2105551043
+// Kelompok : 8
+
 #include <stdio.h>
 
-int main()
-{
-    int a, b, hasil, menu,c, d, hasilbagi;
-    int choice;
-    prog_kalkulator:
-    printf("====================================\n");
-    printf("Program Kalkulator Sederhana\n");
-    printf("====================================\n\n");
-    printf("\tMenu :\n");
-    printf("\t1. Penjumlahan\n");
-    printf("\t2. Pengurangan\n");
-    printf("\t3. Perkalian\n");
-    printf("\t4. Pembagian\n");
-    printf("\t5. Modulus\n");
+int cetak_kalkulasi(int kalkulasi){
 
-    printf("Pilih menu : ");
-    scanf("%d",&menu);
-    printf("-------------------------------\n");
-    switch (menu){
-      case 1:
-        printf("\n1. Penjumlahan :\n");
-        printf("Masukan bilangan pertama : ");
-        scanf("%d", &a);
-        printf("Masukan bilangan kedua   : ");
-        scanf("%d", &b);
-        hasil = a + b;
-        printf("------------------------------- +\n");
-        printf("Hasil:\t\t\t   %d\n", hasil);
-        break;
-    case 2:
-        printf("\n1. Pengurangan :\n");
-        printf("Masukan bilangan pertama : ");
-        scanf("%d", &a);
-        printf("Masukan bilangan kedua   : ");
-        scanf("%d", &b);
-        hasil = a - b;
-        printf("------------------------------- -\n");
-        printf("Hasil:\t\t\t   %d\n", hasil);
-        break;
-    case 3:
-        printf("\n1. Perkalian :\n");
-        printf("Masukan bilangan pertama : ");
-        scanf("%d", &a);
-        printf("Masukan bilangan kedua   : ");
-        scanf("%d", &b);
-        hasil = a * b;
-        printf("------------------------------- x\n");
-        printf("Hasil:\t\t\t   %d\n", hasil);
-        break;
-    case 4:
-        printf("\n1. Pembagian :\n");
-        printf("Masukan bilangan pertama : ");
-        scanf("%f", &c);
-        printf("Masukan bilangan kedua   : ");
-        scanf("%f", &d);
-        hasilbagi = c / d;
-        printf("------------------------------- :\n");
-        printf("Hasil:\t\t\t   %.2f\n", hasilbagi);
-        break;
-    case 5:
-    	printf("\n1. modulus:\n");
-        printf("Masukan bilangan pertama : ");
-        scanf("%d", &c);
-        printf("Masukan bilangan kedua   : ");
-        scanf("%d", &d);
-        printf("%d mod %d = %d\n", c, d, (c%d));
-        printf("------------------------------- \n");
-        printf("Hasil:\t\t\t   %.2f\n", hasil);
-        break;
-      default:
-        printf("Anda salah memilih menu.");
-        break;
+    int angka1 = 0;
+    int angka2 = 0;
+    int total;
+
+    printf("\n\t\tInput nilai pertama : ");
+    scanf("%d", &angka1);
+    
+    printf("\t\tInput nilai kedua : ");
+    scanf("%d", &angka2);
+
+    if(kalkulasi == 1){
+        total = angka1 + angka2;
+        
+    }else if(kalkulasi == 2){
+        total = angka1 - angka2;
+        
+    }else if(kalkulasi == 3){
+        total = angka1 * angka2;
+        
+    }else if(kalkulasi == 4){
+        total = angka1 / angka2;
+        
+    }else if(kalkulasi == 5){
+        total = angka1 % angka2;
     }
-    printf("Selesai\n");
-	printf("__________________________________________\n");
-	printf("Apakah Anda Ingin Melanjutkan\n");
-	printf("Ketik 1 Untuk Melanjutkan\n");
-	printf("Ketik 2 Untuk Keluar\n");
-	scanf("%d", &choice);
-	if(choice ==1) goto prog_kalkulator;
-	else goto exit;
-	exit: 
-	printf("Terimakasih");
- 	return 0;
+    printf("\t\t__________________________________________________________________\n");					
+    printf("\n\t\t\t\t\tHasil Kalkulasi = %d\n\n", total);
+   	printf("\t\t__________________________________________________________________\n");		
+	   			
+}
+void ulangi(){
+    char pil[10];
+    
+	fflush(stdin);
+	printf ("\nIngin Kembali ke Program? (y/n) : "); 
+	scanf("%s", &pil);
  	
+if(!strcmp(pil, "y") || !strcmp(pil, "Y")){
+	system("cls");
+	main();
+	}
+	else if(!strcmp(pil, "n") || !strcmp(pil, "N")){
+		printf("Terimakasih Telah Menggunakan Program Ini!");
+	}  
+	else{
+		printf("Input yang Dimasukkan Salah!\n");
+		ulangi();
+	}
+}
+
+int main(){
+	int kalkulasi;
+	int pil;
+	
+	tampilan:
+	
+	printf("\t\t__________________________________________________________________\n");	
+	printf("\t\t ________________________________________________________________\n");
+	printf("\t\t||                                                              ||\n");					
+	printf("\t\t||                    KALKULATOR  SEDERHANA                     ||\n");					
+	printf("\t\t||..............................................................||\n");					
+	printf("\t\t||                     Pilih Kalkulasi Anda                     ||\n");								
+	printf("\t\t||______________________________________________________________||\n");	
+	printf("\t\t__________________________________________________________________\n");					
+	printf("\t\t* 1.Penjumlahan\n");															
+	printf("\t\t* 2.Pengurangan\n");															
+	printf("\t\t* 3.Perkalian\n");	
+	printf("\t\t* 4.Pembagian\n");
+	printf("\t\t* 5.Modulus\n");
+	printf("\t\t..................................................................\n");		
+	printf("\t\tPilih salah satu kalkulasi yang akan dijalankan : ");
+	scanf("%d", &kalkulasi);
+	printf("\t\t..................................................................\n");	
+	
+	if(kalkulasi == 1 || kalkulasi == 2 || kalkulasi == 3 || kalkulasi == 4 || kalkulasi == 5){
+	cetak_kalkulasi(kalkulasi);
+	}else{
+	printf("\t\tKalkulasi tidak tersedia!");
+	}
+
+	ulangi();
+	
+	return 0;
 }
