bb4de2ec5b2f9b70459623fac2dfbb80a3148921
anindyapramesti <99300812+anindyapramesti@users.noreply.github.com>
2022-05-24 16:44:19 +0800

    Add files via upload

--- Modul4/5_Ranking/5_Ranking.c
+++ Modul4/5_Ranking/5_Ranking.c
@@ -2,60 +2,99 @@
 #include <string.h>
 #include <stdlib.h>
 
-int sorting();
+int Validasisorting();
 int readAsistenin();
 int main();
 void keluar();
-void pilihan_ulang();
-void sortData();
+void pilih_ulang();
+void sortingData();
 void output();
-void menu_utama();
+
 
 struct mahasiswa{
 	char nama[100];
 	int nilai;
 };
 
+int main(){
+   	struct mahasiswa siswa[50];
+	struct mahasiswa temp;	
+	int a = 0;
+	int i, j, k;
+	int hasil;
+	char tempNama[100];
+	int tempNilai;
+
+	printf("\t\t==================================\n");
+	printf("\t\t       	 PROGRAM KUIZ          \n");
+	printf("\t\t            OLEH:                \n");
+	printf(" \t\t         KELOMPOK 08             \n");
+	printf("\t\t==================================\n");
+    printf(" \t\tProses file asistenin.txt...");
+    sleep(1);
+	printf("\n\n");
+
+	a = readAsistenin(siswa);
+
+	printf("\n");
+	hasil = Validasisorting();
+
+	if(hasil == 1){
+		sortingData(siswa, a);
+		printf("\n");
+		output(siswa, a);
+
+		printf("\t\tSelamat! Data berhasil dimasukan ke dalam asistenout.txt\n");
+	}
+	printf("\t\t============================================================\n");
+	pilih_ulang();
+    return 0;
+}
+
 void keluar(){
-    system("cls");
-    system("exit");
+    system("cls || clear");
+	printf("\t\t==================================\n");
+	printf("\t\t                                  \n");
+	printf("\t\t         TERIMAKASIH BANYAK        \n");
+	printf("\t\t                                  \n");
+	printf("\t\t==================================\n");
 }
 
 
-void pilihan_ulang(){
-    char huruf_ulang[1];
+void pilih_ulang(){
+    char ulang[1];
 
-    printf("\n \t\tLakukan lagi? ya/keluar(Y/E) : ");
-    scanf("%s", &huruf_ulang);
+    printf("\n \t\tLakukan Ulang? ya/keluar(Y/E) : ");
+    scanf("%s", ulang);
     fflush(stdin);
 
-    if((strcmp(huruf_ulang, "Y") == 0) || (strcmp(huruf_ulang, "y") == 0)){
+    if((strcmp(ulang, "Y") == 0) || (strcmp(ulang, "y") == 0)){
         system("clear || cls");
-		    menu_utama();
-    }else if((strcmp(huruf_ulang, "E") == 0) || (strcmp(huruf_ulang, "e") == 0)) {
+		int main();
+    }else if((strcmp(ulang, "E") == 0) || (strcmp(ulang, "e") == 0)) {
         system("clear || cls");
-		    keluar();
+		keluar();
     }else{
-    	  printf("\t\tMasukan anda salah! Masukan dengan benar\n");
-        pilihan_ulang();
+    	printf("\t\tTidak Valid! Silahkan masukan kembali\n");
+        pilih_ulang();
     }
 }
 
-int sorting(){
-    char huruf_sort[10];
-	  int hasil = 0;
+int Validasisorting(){
+    char pilihan_sort[10];
+	int hasil = 0;
 
-    printf("\t\tApakah anda ingin melakukan mengurutkan nilai ? (Y/N) : ");
-    scanf("%s", huruf_sort);
+    printf("\t\tLakukan Pengurutan Nilai? (Y/N) : ");
+    scanf("%s", pilihan_sort);
     fflush(stdin);
 
-    if((strcmp(huruf_sort, "N") == 0) || (strcmp(huruf_sort, "n") == 0)){
-		    hasil = 0;
-    }else if((strcmp(huruf_sort, "Y") == 0) || (strcmp(huruf_sort, "y") == 0)){
-		    hasil = 1;
+    if((strcmp(pilihan_sort, "N") == 0) || (strcmp(pilihan_sort, "n") == 0)){
+		hasil = 0;
+    }else if((strcmp(pilihan_sort, "Y") == 0) || (strcmp(pilihan_sort, "y") == 0)){
+		hasil = 1;
     }else{
-		    printf("\t\t Masukan anda salah! Masukan dengan benar\n\n");
-		    return sorting();
+		printf("\t\t Tidak Valid! Silahkan masukan kembali\n\n");
+		return Validasisorting();
     }
 
 	return hasil;
@@ -66,14 +105,15 @@ int readAsistenin(struct mahasiswa siswa[]){
 	FILE *in = fopen("asistenin.txt","r");
 	if(!in){
 		printf("\t\tMaaf! File asistenin.txt tidak ditemukan\n");
-		printf(" \t\tSilahkan buat file asistenin.txt terlebih dahulu\n");
+		printf(" \t\tPastikan telah membuat file asistenin.txt terlebih dahulu\n");
+		abort();
 	}else{
-    printf("\t\tNilai Mahasiswa : \n");
+        printf("\t\tNilai Mahasiswa : \n");
 		while(!feof(in)){
 			printf("    ");
 		    fscanf(in,"%[^\n]s\n", siswa[a].nama);
 		    fscanf(in,"%d\n", &siswa[a].nilai);
-        fflush(stdin);
+            fflush(stdin);
 		    printf("\t\t%d. %s : %d\n", a+1, siswa[a].nama, siswa[a].nilai);
 		    a++;
 		}
@@ -83,7 +123,7 @@ int readAsistenin(struct mahasiswa siswa[]){
 	return a;
 }
 
-void sortData(struct mahasiswa siswa[], int a){
+void sortingData(struct mahasiswa siswa[], int a){
 	struct mahasiswa temp;
 	int i, j;
 	for(i = 0; i < a - 1; i++){
@@ -93,7 +133,7 @@ void sortData(struct mahasiswa siswa[], int a){
 				siswa[j+1] = siswa[j];
     			siswa[j] = temp;
 			}
-    }
+    	}
 	}
 }
 
@@ -107,45 +147,5 @@ void output(struct mahasiswa siswa[], int a){
 	fclose(out);
 }
 
-void menu_utama(){
-	struct mahasiswa siswa[50];
-	struct mahasiswa temp;	
-	int a = 0;
-	int i, j, k;
-	int sort;
-	char tempNama[100];
-	int tempNilai;
 
-	printf("\t\t__________________________________________________________________\n");	
-	printf("\t\t ________________________________________________________________\n");
-	printf("\t\t||                                                              ||\n");					
-	printf("\t\t||                  	Program Ranking	               		||\n");					
-	printf("\t\t||..............................................................||\n");					
-	printf("\t\t||                    	By kelompok 08                      	||\n");								
-	printf("\t\t||______________________________________________________________||\n");	
-	printf("\t\t__________________________________________________________________\n");	
-  printf(" \t\tProses file asistenin.txt...");
-  sleep(1);
-	printf("\n\n");
 
-	a = readAsistenin(siswa);
-
-	printf("\n");
-	sort = sorting();
-
-	if(sort == 1){
-		sortData(siswa, a);
-		printf("\n");
-		output(siswa, a);
-
-		printf("\t\tData berhasil dimasukan ke dalam asistenout.txt\n");
-	}
-	printf("\t\t============================================================\n");
-	pilihan_ulang();
-
-}
-
-int main(){
-    menu_utama();
-    return 0;
-}
