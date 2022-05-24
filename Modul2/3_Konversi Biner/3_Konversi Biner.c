42e736c89e3c825267b1a7e4df2e488054aaf354
anindyapramesti <99300812+anindyapramesti@users.noreply.github.com>
2022-05-24 16:39:39 +0800

    Add files via upload

--- Modul2/3_Konversi Biner/3_Konversi Biner.c
+++ Modul2/3_Konversi Biner/3_Konversi Biner.c
@@ -5,120 +5,120 @@
  int validasiDesimal();
  int validasiBiner();
  void ulangi();
- int validasiInt();
+ int validasiAwal();
 
 int main(){
+	
 	int pilihan, nilai;
 	
 	tampilan:
 	
-	printf("\t\t__________________________________________________________________\n");	
-	printf("\t\t ________________________________________________________________\n");
-	printf("\t\t||                                                              ||\n");					
-	printf("\t\t||                  Program Konversi Bilangan	                ||\n");					
-	printf("\t\t||..............................................................||\n");					
-	printf("\t\t||                     Pilih Pilihan Anda                       ||\n");								
-	printf("\t\t||______________________________________________________________||\n");	
-	printf("\t\t__________________________________________________________________\n");					
-	printf("\t\t* 1.Biner ke Desimal\n");															
-	printf("\t\t* 2.Desimal ke Biner\n");															
-	printf("\t\t..................................................................\n");		
-	printf("\t\tPilih salah satu yang akan dijalankan : ");
-	pilihan=validasiInt();
-	printf("\t\t..................................................................\n");	
+	printf("==================================\n");
+	printf("       KONVERSI  BILANGAN         \n");
+	printf("             OLEH:                \n");
+	printf("          KELOMPOK 08             \n");
+	printf("==================================\n");		
+	printf("* 1.Biner ke Desimal\n");															
+	printf("* 2.Desimal ke Biner\n");															
+	printf("................................\n");		
+	printf("Pilih salah satu yang akan dijalankan : ");
+	pilihan=validasiAwal();
+	printf(".......................................\n");	
 	
 	if(pilihan == 1 || pilihan == 2){
-   		cetak_pilihan(pilihan);
+   		 cetak_pilihan(pilihan);
 	} else {
    		 return nilai;
 	}
-		ulangi();
+
+	ulangi();
+	
 	}
 
-int validasiInt() {
-    	int nilai;
-	char check;
+int validasiAwal() {
+    int angka;
+	char cek;
 	
-		if (scanf("%i%c", &nilai, &check) != 2 || check != '\n') {
-		while((check = getchar()) != '\n' && check != EOF){
-			}
-			printf("\n\t\tNilai yang anda masukan tidak valid! Masukan ulang: ");
-		        return validasiInt();
-		        printf("\n\t\t===============================================\n");
-		} else if (nilai <= 0 || nilai >2) {
-			printf("\n\t\tNilai yang anda masukan tidak valid! Pilih 1-2: ");
-			return validasiInt();
-	       		printf("\n===============================================\n");	
+	    if (scanf("%i%c", &angka, &cek) != 2 || cek != '\n') {
+	    	while((cek = getchar()) != '\n' && cek != EOF){
+	        }
+			printf("\nNilai yang anda masukan tidak valid! Masukan ulang: ");
+		    return validasiAwal();
+		    printf("\n===============================================\n");
+		} else if (angka <= 0 || angka >2) {
+			printf("\nNilai yang anda masukan tidak valid! Pilih 1-2: ");
+			return validasiAwal();
+	        printf("\n===============================================\n");	
 		} else {
-	        	return nilai;
+	        return angka;
 	    }
 	}
 
 int validasiDesimal(){
-	int nilai;
-	char check;
+	int angka;
+	char cek;
 	
-	    if (scanf("%i%c", &nilai, &check) != 2 || check != '\n') {
-	    	while((check = getchar()) != '\n' && check != EOF){
+	    if (scanf("%i%c", &angka, &cek) != 2 || cek != '\n') {
+	    	while((cek = getchar()) != '\n' && cek != EOF){
 	        }
-			printf("\n\t\tNilai yang anda masukan tidak valid! Masukan ulang: ");
-		        return validasiDesimal();
-	        	printf("\n===============================================\n");
-		} else if (nilai < 0) {
-			printf("\n\t\tNilai yang anda masukan tidak valid! Input antara 0-100: ");
+			printf("\nNilai yang anda masukan tidak valid! Masukan ulang: ");
+		    return validasiDesimal();
+	        printf("\n===============================================\n");
+		} else if (angka < 0) {
+			printf("\nNilai yang anda masukan tidak valid! Input antara 0-100: ");
 			return validasiDesimal();	
 			printf("\n===============================================\n");
 		} else {
-	        	return nilai;
+	        return angka;
 	    }
 	}   
 
 int validasiBiner(){
-	int nilai;
-	char check;
+	int angka;
+	char cek;
 	int temp=0;
 	
-	    if (scanf("%i%c", &nilai, &check) != 2 || check != '\n') {
-	        while((check = getchar()) != '\n' && check != EOF){
+	    if (scanf("%i%c", &angka, &cek) != 2 || cek != '\n') {
+	        while((cek = getchar()) != '\n' && cek != EOF){
 	        }
-		        printf("\n\t\tNilai yang anda masukan tidak valid! Masukan ulang: ");
+		        printf("\nNilai yang anda masukan tidak valid! Masukan ulang: ");
 		        return validasiBiner();
 		        printf("\n===============================================\n");
 	    } else {
-		      int a=nilai;
+	      int a=angka;
+	       
+	      while (angka != 0) {
+	        temp = angka % 10;
+	          if ((temp == 0) || (temp == 1)) {
+	                      return a;
+	              } else {
+	                  printf("\nNilai yang anda masukan tidak valid! Input bilangan biner menggunakan 0 dan 1: ");
+	            return validasiBiner();	
+	            printf("\n===============================================\n");
+	              }
+	          }
+	      }
+	   return angka; 
+	} 
 
-		      while (nilai != 0) {
-			temp = nilai % 10;
-			  if ((temp == 0) || (temp == 1)) {
-				      return a;
-			     } else {
-				printf("\n\t\tNilai yang anda masukan tidak valid! Input bilangan biner menggunakan 0 dan 1: ");
-				return validasiBiner();	
-			    	printf("\n===============================================\n");
-			      }
-			  }
-		 }
-	return nilai; 
-} 
+
 
 int cetak_pilihan(int pilihan){
 	int biner, desimal, nilai;
 	
 	system("cls || clear");
-	
+    
 	    if (pilihan==1){
-		printf("\t\t__________________________________________________________________\n");	
-		printf("\t\t ________________________________________________________________\n");
-		printf("\t\t||                                                              ||\n");					
-		printf("\t\t||                   Program Konversi Bilangan	                ||\n");					
-		printf("\t\t||..............................................................||\n");					
-		printf("\t\t||                     BINER KE DESIMAL	                        ||\n");								
-		printf("\t\t||______________________________________________________________||\n");	
-		printf("\t\t__________________________________________________________________\n");			
-		printf("\t\tMasukan bilangan biner: ");
+			 	printf("==================================\n");
+				printf("                                  \n");
+				printf("         BINER KE DESIMAL         \n");
+				printf("                                  \n");
+				printf("==================================\n");				
+				printf("Masukan bilangan biner: ");
 				
-		biner=validasiBiner();
-		int  desimal = 0, base = 1, sisa;  
+				biner=validasiBiner();
+				
+				int  desimal = 0, base = 1, sisa;  
 	 
 			    while (biner > 0)  
 			    {  
@@ -127,69 +127,64 @@ int cetak_pilihan(int pilihan){
 			        biner = biner / 10; 
 			        base = base * 2;  
 			    }  
-			    printf("\t\t==================================\n");
+			    printf("==================================\n");
 				printf("                                  \n");
-				printf("\t\t   Hasil konversi adalah: %d\n", desimal);
+				printf("   Hasil konversi adalah: %d\n", desimal);
 				printf("                                  \n");
-				printf("\t\t==================================\n");	
+				printf("==================================\n");	
 		
 			
-	    } if (pilihan==2) {
-		printf("\t\t__________________________________________________________________\n");	
-		printf("\t\t ________________________________________________________________\n");
-		printf("\t\t||                                                              ||\n");					
-		printf("\t\t||                   Program Konversi Bilangan	                ||\n");					
-		printf("\t\t||..............................................................||\n");					
-		printf("\t\t||                     DESIMAL KE BINER	                        ||\n");								
-		printf("\t\t||______________________________________________________________||\n");	
-		printf("\t\t__________________________________________________________________\n");			
-		printf("\t\tMasukan bilangan desimal: ");	
+		} if (pilihan==2) {
+				printf("==================================\n");
+				printf("                                  \n");
+				printf("         DESIMAL KE BINER         \n");
+				printf("                                  \n");
+				printf("==================================\n");			
+				printf("\t\tMasukan bilangan desimal: ");	
 				
-		desimal=validasiDesimal();
-		int array[10],i;
+				desimal=validasiDesimal();
+				int array[10],i;
 	
-			for(i=0; desimal>0; i++){
+			    for(i=0; desimal>0; i++){
 			        array[i]= desimal%2;    
 			        desimal = desimal/2;    
 			    }
-				printf("\t\t==================================\n");
-				printf("                                  ");
-				printf("\n\t\tBilangan biner adalah: ");
+					printf("==================================\n");
+					printf("                                  ");
+				    printf("\nBilangan biner adalah: ");
 			
 			    for(i=i-1; i>=0; i--){
 			        printf("%d",array[i]); 
 			    }
-				printf("\n                                  ");
-				printf("\n\t\t==================================\n");
+				    printf("\n                                  ");
+					printf("\n==================================\n");
 				    return 0;
-		} else {
-			return nilai;
-			}
-	}
+				} else {
+					return nilai;
+				}
+		}
 		
+
 void ulangi(){
     char pil[10];
-    char check;
+    char cek;
     
-		while((check = getchar()) != '\n' && check != EOF){
+		while((cek = getchar()) != '\n' && cek != EOF){
 	        }
-			printf ("\n\t\tIngin Kembali ke Program? (y/n) : "); 
-			scanf("%s", pil);
+				printf ("\nIngin Kembali ke Program? (y/n) : "); 
+				scanf("%s", pil);
 		if(!strcmp(pil, "y") || !strcmp(pil, "Y")){
-			system("cls || clear");
-			main();
+				system("cls || clear");
+				main();
 		}else if(!strcmp(pil, "n") || !strcmp(pil, "N")){
-			system("cls || clear");
-			printf("\t\t__________________________________________________________________\n");	
-			printf("\t\t ________________________________________________________________\n");
-			printf("\t\t||                                                              ||\n");					
-			printf("\t\t||           TERIMAKASIH TELAH MENGGUNAKAN PROGRAM INI          ||\n");						
-			printf("\t\t||..............................................................||\n");				
-			printf("\t\t||                     	BY KELOMPOK 08	                        ||\n");								
-			printf("\t\t||______________________________________________________________||\n");	
-			printf("\t\t__________________________________________________________________\n");
+				system("cls || clear");
+				printf("==================================\n");
+				printf("                                  \n");
+				printf("         TERIMAKASIH BANYAK        \n");
+				printf("                                  \n");
+				printf("==================================\n");	
 		}else{
-			printf("\t\tInput yang Dimasukkan Salah!\n");
-			ulangi();
+				printf("Input yang Dimasukkan Salah!\n");
+				ulangi();
 		}
 	}
